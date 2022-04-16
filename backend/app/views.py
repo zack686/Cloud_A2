95a6d69d3c40fbc1c036705fd66d1f12bf7a82d0
zack686 <chenmccaig@gmail.com>
2022-04-16 19:34:59 +1000

    added api to search tweets

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,5 +1,45 @@
 from app import app
+from tweepy import Client
 
 @app.route("/")
 def index():
-    return "hello world"
+    return "Cloud A2"
+
+@app.route("/search")
+def twetterSearch():
+    bearer_token = "WIP"
+
+    if not bearer_token:
+        raise RuntimeError("Not found bearer token")
+    
+    client = Client(bearer_token)
+
+    # https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
+    query = "melbourne"
+
+    max_results = 10
+    limit = 30
+    counter = 0
+    tweets = {}
+
+    # https://docs.tweepy.org/en/stable/client.html#search-tweets
+    resp = client.search_recent_tweets(query, max_results=max_results)
+    if resp.errors:
+        raise RuntimeError(resp.errors)
+    if resp.data:
+        for tweet in resp.data:
+            print(tweet.__repr__())
+            tweets[counter] = tweet.__repr__()
+            counter += 1
+    
+    # while resp.meta["next_token"] and counter < limit:
+    #     resp = client.search_recent_tweets(query, max_results=max_results, next_token=resp.meta["next_token"])
+    #     if resp.errors:
+    #         raise RuntimeError(resp.errors)
+    #     if resp.data:
+    #         for tweet in resp.data:
+    #             print(tweet.__repr__())
+    #            # tweets += tweet.__repr__()
+    #             tweets[counter] = tweet.__repr__()
+    #             counter += 1
+    return tweets
--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,10 @@
-DEBUG=True
+from os import environ
+from dotenv import load_dotenv
+
+
+load_dotenv('.env')
+#SECRET_KEY = environ.get('TWITTER_BEARER_TOKEN')
+
+DEBUG=environ.get('DEBUG')
+class TwitterAPI:
+    TWITTER_BEARER_TOKEN = environ.get('TWITTER_BEARER_TOKEN')
