663cdb8aa65416dd3f083a7a63008d7d18a2dd29
JuliusKer <julius.kerstien@student.unimelb.edu.au>
2022-04-20 12:19:12 +1000

    restx setup

--- backend/apis/twitterSearch.py
+++ backend/apis/twitterSearch.py
@@ -2,45 +2,92 @@ from apis import app
 from config import TwitterAPI
 from tweepy import Client
 
+# from apispec import APISpec
+# from apispec.ext.marshmallow import MarshmallowPlugin
+# from apispec_webframeworks.flask import FlaskPlugin
+# from flask import Flask, jsonify, render_template, send_from_directory
+# from marshmallow import Schema, fields
+from flask import Flask
+from flask_restx import Resource, Api
+
+api = Api(app)
+
 @app.route("/")
 def index():
     return "Cloud A2"
 
-@app.route("/search")
-def twetterSearch():
-    bearer_token = app.config["TWITTER_BEARER_TOKEN"]
+@api.route("/cloud")
+class Cloud(Resource):
+    def get(self):
+        return "Cloud A2"
 
-    if not bearer_token:
-        raise RuntimeError("Not found bearer token")
+@api.route("/search")
+class TwitterSeach(Resource):
+    def get(self):
+
+        bearer_token = app.config["TWITTER_BEARER_TOKEN"]
+
+        if not bearer_token:
+            raise RuntimeError("Not found bearer token")
+        
+        client = Client(bearer_token)
+
+        # https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
+        query = "melbourne"
+
+        max_results = 10
+        limit = 30
+        counter = 0
+        tweets = {}
+
+        # https://docs.tweepy.org/en/stable/client.html#search-tweets
+        resp = client.search_recent_tweets(query, max_results=max_results)
+        if resp.errors:
+            raise RuntimeError(resp.errors)
+        if resp.data:
+            for tweet in resp.data:
+                print(tweet.__repr__())
+                tweets[counter] = tweet.__repr__()
+                counter += 1
+        
+        return tweets
+
+# @app.route("/search")
+# def twetterSearch():
+
+#     bearer_token = app.config["TWITTER_BEARER_TOKEN"]
+
+#     if not bearer_token:
+#         raise RuntimeError("Not found bearer token")
     
-    client = Client(bearer_token)
+#     client = Client(bearer_token)
 
-    # https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
-    query = "melbourne"
+#     # https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
+#     query = "melbourne"
 
-    max_results = 10
-    limit = 30
-    counter = 0
-    tweets = {}
+#     max_results = 10
+#     limit = 30
+#     counter = 0
+#     tweets = {}
 
-    # https://docs.tweepy.org/en/stable/client.html#search-tweets
-    resp = client.search_recent_tweets(query, max_results=max_results)
-    if resp.errors:
-        raise RuntimeError(resp.errors)
-    if resp.data:
-        for tweet in resp.data:
-            print(tweet.__repr__())
-            tweets[counter] = tweet.__repr__()
-            counter += 1
+#     # https://docs.tweepy.org/en/stable/client.html#search-tweets
+#     resp = client.search_recent_tweets(query, max_results=max_results)
+#     if resp.errors:
+#         raise RuntimeError(resp.errors)
+#     if resp.data:
+#         for tweet in resp.data:
+#             print(tweet.__repr__())
+#             tweets[counter] = tweet.__repr__()
+#             counter += 1
     
-    # while resp.meta["next_token"] and counter < limit:
-    #     resp = client.search_recent_tweets(query, max_results=max_results, next_token=resp.meta["next_token"])
-    #     if resp.errors:
-    #         raise RuntimeError(resp.errors)
-    #     if resp.data:
-    #         for tweet in resp.data:
-    #             print(tweet.__repr__())
-    #            # tweets += tweet.__repr__()
-    #             tweets[counter] = tweet.__repr__()
-    #             counter += 1
-    return tweets
+#     # while resp.meta["next_token"] and counter < limit:
+#     #     resp = client.search_recent_tweets(query, max_results=max_results, next_token=resp.meta["next_token"])
+#     #     if resp.errors:
+#     #         raise RuntimeError(resp.errors)
+#     #     if resp.data:
+#     #         for tweet in resp.data:
+#     #             print(tweet.__repr__())
+#     #            # tweets += tweet.__repr__()
+#     #             tweets[counter] = tweet.__repr__()
+#     #             counter += 1
+#     return tweets
--- backend/config.py
+++ backend/config.py
@@ -7,3 +7,6 @@ load_dotenv('.env')
 
 class TwitterAPI:
     TWITTER_BEARER_TOKEN = environ.get('TWITTER_BEARER_TOKEN')
+
+#class TwitterAPI:
+#    TWITTER_BEARER_TOKEN = environ.get('AAAAAAAAAAAAAAAAAAAAAJspbgEAAAAADFM%2BOs5zS8ib9LHFvzGw8byssBg%3DoFHQ4m8azb9ba0vs6KcZ41joi0NS42daexGqziCdS3RlZ3fDjj')
