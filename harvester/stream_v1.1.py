8e0b397d71a8966d5f3bf19af245bb7013560b08
zack686 <chenmccaig@gmail.com>
2022-05-01 22:17:58 +1000

    update v1.1

--- harvester/stream_v1.1.py
+++ harvester/stream_v1.1.py
@@ -1,10 +1,11 @@
 import tweepy
 import json
-# Subclass Stream to print IDs of Tweets received
-consumer_key = "3bYkLieVfNVSsez3IE0U84JwH"
-consumer_secret = "FA0GQQy2OKRRfQi7Oej7QpDKAnTXvDGZDBD5F10ra5WyH1EOp6"
-access_token = "1514846543748079616-YIPxVajLpxK8Bw8t9I5lYeR4AJ44Qe"
-access_token_secret = "EjTFLMHeG0aEUPLxW3PdDFs9AgayZwx85OnbGuWLgn8Vg"
+
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
 
 class Stream(tweepy.Stream):
   def on_data(self, raw_data):
