130887bbce2b204101647d2cfe1771193c77763d
zack686 <chenmccaig@gmail.com>
2022-05-01 18:35:27 +1000

    working version of stream

--- src/sample scripts/tweepy-stream-example.py
+++ src/sample scripts/tweepy-stream-example.py
@@ -4,35 +4,18 @@ import tweepy
 
 consumer_key = "UG5SdYVvFUllQjGmbaujH1sjE"
 consumer_secret = "ZNTk1vEgLplByLIosOkJo7011a4OeTR9yeQsnS5hUFLawQnYsx"
-access_token = "1513036168199553031-pdsr3Bip4XwELmSre9P7uck1pl3xl5"
-access_token_secret = "9cOIV9rHndNov8QD4GSbjS0QUjls03KJuEhCR4bt5STnX"
+access_token = "1514846543748079616-YIPxVajLpxK8Bw8t9I5lYeR4AJ44Qe"
+access_token_secret = "EjTFLMHeG0aEUPLxW3PdDFs9AgayZwx85OnbGuWLgn8Vg"
 
-# Subclass Stream to print IDs of Tweets received
-class IDPrinter(tweepy.Stream):
-
-    def on_status(self, status):
-        print(status.id)
-
-# Initialize instance of the subclass
-# printer = IDPrinter(
-#   consumer_key, consumer_secret,
-#   access_token, access_token_secret
-# )
-
-# # Filter realtime Tweets by keyword
-# printer.filter(track=["Melbourne"])
-
-# streaming_client = tweepy.StreamingClient("AAAAAAAAAAAAAAAAAAAAAEsNbgEAAAAA1X6WSK4PEKbw82NXCWGKCgD%2Bjt4%3DjXHskBEH0Fbt2ipcW79cwjZ1ScACNLrz6kTeCHylMkyn2b6Aks")
-# # streaming_client.sample()
-# streaming_client.add_rules(tweepy.StreamRule("Melbourne"))
-# streaming_client.filter()
-
-
-import os
-import logging
 import json
 from tweepy import StreamingClient, StreamRule, Tweet
 
+
+auth = tweepy.OAuth1UserHandler(
+   consumer_key, consumer_secret, access_token, access_token_secret
+)
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 class TweetListener(StreamingClient):
     """
@@ -45,33 +28,11 @@ class TweetListener(StreamingClient):
         # json_data = json.loads(tweet)  # convert string to JSON
         # print(json_data)
         # print(tweet.id)
-        print(type(tweet))
-        # print(tweet._json["created_at"])
-        print(tweet.geo)
-        # print(tweet)
-    # def on_status(self, tweet_mode="extended"):
-    #     print(data)
+        pass
     
-    # print full tweet data
-    # def on_status(self, status):
-    #     if hasattr(status, "retweeted_status"):  # Check if Retweet
-    #         try:
-    #             print(status.retweeted_status.extended_tweet["full_text"])
-    #         except AttributeError:
-    #             print(status.retweeted_status.text)
-    #     else:
-    #         try:
-    #             print(status.extended_tweet["full_text"])
-    #         except AttributeError:
-    #             print(status.text)
-    # def on_data(self, raw_data):
-    #      # self.tweet_count += 1  # track number of tweets processed
-    #     json_data = json.loads(raw_data)  # convert string to JSON
-    #     # print(json_data["data"]["id"])
-    #     # self.db.tweets.insert_one(json_data)  # store in tweets collection
-    #     # print(json_data) 
-    #     # print(f'     Created at: {json_data["created_at"]}')         
-    #     # print(f'Tweets received: {self.tweet_count}')  
+    def on_data(self, raw_data):
+        data = json.loads(raw_data)
+        print(data)
 
     def on_request_error(self, status_code):
         print(status_code)
@@ -131,9 +92,7 @@ if __name__ == "__main__":
     
     # https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/api-reference/get-tweets-search-stream
     try:
-        # client.filter()
-        stream = tweepy.Stream(auth=api.auth, listener=LiveTweetListener(), tweet_mode='extended')
-
+        client.filter(expansions="geo.place_id")
         
     except KeyboardInterrupt:
         client.disconnect()
