01fe9ee5af5aa9a0d7632d957cc1551a7769dff4
JKerlidis <jamesk@ozemail.com.au>
2022-05-04 02:16:23 +1000

    Fix typo

--- tweetProcessor/main.py
+++ tweetProcessor/main.py
@@ -1,9 +1,9 @@
 import os
 
-from src.couchDB import connect_to_database
+from src.couchDB import connect_to_database_partition
 
 
-db = connect_to_database(
+db = connect_to_database_partition(
     os.environ["COUCHDB_USERNAME"],
     os.environ["COUCHDB_PASSWORD"],
     os.environ["COUCHDB_NODE_IP"],
