f1e3fc84f4b530e3599d34ecbf39447a639c73a5
JKerlidis <jamesk@ozemail.com.au>
2022-05-06 00:05:34 +1000

    Fix mapreduce view

--- tweetProcessor/designDocuments/untagged_geo_tweets_map.js
+++ tweetProcessor/designDocuments/untagged_geo_tweets_map.js
@@ -1,9 +1,9 @@
 function (doc) {
     if (doc.geo) {
         if (doc.id_str) {
-            emit(doc.id_str);
+            emit(doc.id_str, 1);
         } else if (doc.id) {
-            emit(doc.id.toString());
+            emit(doc.id.toString(), 1);
         }
     }
 }
