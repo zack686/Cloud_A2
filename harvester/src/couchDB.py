0f6d3b1c41d241e668f9ee419d494b18eea11cfa
JKerlidis <jamesk@ozemail.com.au>
2022-05-11 18:48:24 +1000

    Add tweet processor to deployment

--- harvester/src/couchDB.py
+++ harvester/src/couchDB.py
@@ -40,46 +40,34 @@ def transform_extracted_tweets(tweet_list: List[dict]):
         tweet_list[i] = tweet
 
 
-def put_tweet(server: couchdb.Server, tweet: dict, allow_unidentified: bool=False,
-    set_unprocessed: bool=True) -> tuple:
+def put_tweet(server: couchdb.Server, tweet: dict, allow_unidentified: bool=False) -> tuple:
     """ Put one tweet in the 'twitter' database of a given couchdb server,
     doing nothing if there is already a tweet in the database with the given
     id. In the case of a successful put, returns a tuple consisting of the
     document id and revision number.
     
     Tweets with no 'id' or 'id_str' key are assigned a random UUID if
-    `allow_unidentified` is set to true and rejected otherwise, while ids are
-    additionally saved to an 'unprocessed' database if the `set_unprocessed`
-    key is set to true. """
+    `allow_unidentified` is set to true and rejected otherwise. """
 
     set_tweet_id(tweet)
     if "unidentified" in tweet and not allow_unidentified:
         return
 
     try:
-        result = server["twitter"].save(tweet)
+        return server["twitter"].save(tweet)
     except couchdb.http.ResourceConflict:
         return
 
-    if set_unprocessed:
-        try:
-            server["unprocessed"].save({"_id": result[0]})
-        except couchdb.http.ResourceConflict:
-            pass
 
-    return result
-
-
-def bulk_put_tweets(server: couchdb.Server, tweet_list: List[dict], allow_unidentified: bool=False,
-    set_unprocessed: bool=True) -> List[tuple]:
+def bulk_put_tweets(server: couchdb.Server, tweet_list: List[dict],
+    allow_unidentified: bool=False) -> List[tuple]:
     """ Put multiple tweets in the 'twitter' database of a given couchdb
     server, ignoring tweets with an id already existing in the database.
     Returns a list of tuples consisting of the document id and revision number
     of every tweet that was inserted into the database.
     
     Tweets with no 'id' or 'id_str' key are silently rejected if
-    `allow_unidentified` is set to false, while ids are additionally saved to
-    an 'unprocessed' database if the `set_unprocessed` key is set to true. """
+    `allow_unidentified` is set to falsee. """
 
     for tweet in tweet_list:
         set_tweet_id(tweet)
@@ -88,11 +76,4 @@ def bulk_put_tweets(server: couchdb.Server, tweet_list: List[dict], allow_uniden
         tweet_list = [tweet for tweet in tweet_list if "unidentified" not in tweet]
 
     results = server["twitter"].update(tweet_list)
-    results = [(doc_id, doc_rev) for (success, doc_id, doc_rev) in results if success]
-
-    if set_unprocessed:
-        server["unprocessed"].update(
-            [{"_id": result[0]} for result in results]
-        )
-
-    return results
+    return [(doc_id, doc_rev) for (success, doc_id, doc_rev) in results if success]
---
+++ tweet_processor/main.py
@@ -0,0 +1,45 @@
+import os
+
+import flair
+
+from tweet_processor.src.couchDB import *
+from tweet_processor.src.tweetTagger import *
+
+
+# Connect to the couchdb server
+server = connect_to_couchdb_server(
+    os.environ["COUCHDB_USERNAME"],
+    os.environ["COUCHDB_PASSWORD"],
+    os.environ["COUCHDB_NODE_IP"]
+)
+
+# Get the aurin suburb boundary data
+aurin_db = server["aurin"]
+vic_suburbs = dict(aurin_db.get("vic_states"))
+
+vic_suburb_boundaries = GeoBoundaries(vic_suburbs, "vic_loca_2")
+
+# Load the flair sentiment classifier
+classifier = flair.models.TextClassifier.load("en-sentiment")
+
+# Download and tag geo-located tweets in the specified id range
+insert_db = server["tagged"]
+extract_db = server["twitter"]
+batch_size = 500
+tagged_tweets = []
+tweet_generator = batch_get_tweets_with_prefix(extract_db, batch_size)
+
+for tweet in tweet_generator:
+    if len(tagged_tweets) >= batch_size:
+        output = bulk_put_tweets(insert_db, tagged_tweets)
+        print(output)
+        print(f"Inserted {len(output)} tweets into the database.")
+        tagged_tweets = []
+
+    tagged_tweet = tag_tweet(tweet, vic_suburb_boundaries, classifier)
+    if tagged_tweet is not None:
+        tagged_tweets.append(tagged_tweet)
+
+if tagged_tweets != []:
+    output = bulk_put_tweets(insert_db, tagged_tweets)
+    print(f"Inserted {len(output)} tweets into the database.")
