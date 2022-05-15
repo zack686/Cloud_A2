2b415d86633a95a55d7149777cc2e3f0a8f1d576
JKerlidis <jamesk@ozemail.com.au>
2022-05-15 22:50:53 +1000

    Change suburb tag to suburb id tag

--- tweet_processor/design_docs/map.js
+++ tweet_processor/design_docs/map.js
@@ -2,10 +2,10 @@ function (doc) {
     doc.tokens.split(' ').some(function (word) {
         if (['education', 'vce', 'vcaa', 'atar', 'unit', 'units', 'study', 'class', 'textbook', 'maths', 'english', 'school', 'schools', 'student', 'students', 'result', 'results', 'exam', 'exams', 'assignments', 'assignment'].includes(word)) {
             if (doc.sentiment == 'POSITIVE') {
-                emit(doc.suburb_tag, 1);
+                emit(doc.suburb_id_tag, 1);
             }
             else {
-                emit(doc.suburb_tag, 0);
+                emit(doc.suburb_id_tag, 0);
             }
         }
     });
