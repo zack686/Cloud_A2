adeecc5f78fb6f80ec832c02e5b81caeae0cc851
zack686 <chenmccaig@gmail.com>
2022-05-17 10:46:29 +1000

    removed unused scripts

--- src/sample scripts/connectDB.py
+++
@@ -1,9 +0,0 @@
-import couchdb
-
-
-user = "admin"
-password = "pass123"
-couchserver = couchdb.Server("http://%s:%s@172.26.131.127:5984/" % (user, password))
-
-for dbname in couchserver:
-    print(dbname)
--- src/sample scripts/run_tweepy_search_example.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -a
-. ../config
-set +a
-
-python3 ./tweepy-search-example.py
